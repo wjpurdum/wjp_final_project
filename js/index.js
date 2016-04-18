fda43c41b17c7d33ab6f8624bd97d762fe91fe71
wjpurdum <whitney.purdum@gmail.com>
2016-04-18 18:59:37 -0400

    innit 4

--- js/index.js
+++ js/index.js
@@ -4,30 +4,39 @@ $(document).ready(function() {
 		function showHiddenList() {
 			$(".hidden-list").css('display','block');
 		}
-	
 
-$('#registrationForm').submit(processForm);
-	function processForm(event) {
+
+
+
+
+
+
+			function processForm(event) {
 		event.preventDefault();
 
-		
+
 		var emailAddress = $('input[name="emailAddress"]').val();
-		
+		var lovesSpace = $('input[name="lovesSpace"]:checked').val();
+		var numberOfAstronauts = $('input[name="astronauts"]:checked').length;
+		var planet = $('select[name="planet"]').val();
 
-		if (emailAddress.indexOf('@') === -1) {
+		if (username === '') {
 			$('#errorMessage')
-				.html('يرجى تقديم عنوان بريد إلكتروني صالح')
+				.html('Please enter a username')
 				.removeClass('hide');
-
-		} else if (phone-number === '') {
-			$('#errorMessage')
-				.html('الرجاء تقديم رقم هاتف صالح')
-				.removeClass('hide');
-
-
 		}
 
-	}
+		else if (password.length < 8) {
+			$('#errorMessage')
+				.html('Password must be 8 characters')
+				.removeClass('hide');
+		}
+
+
+		else if (emailAddress.indexOf('@') === -1) {
+			$('#errorMessage')
+				.html('Please provide a valid email address')
+				.removeClass('hide');
 
 	});
 
