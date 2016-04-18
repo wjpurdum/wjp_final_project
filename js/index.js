8a342717a6e9537ed800980f387461923d63c1a1
wjpurdum <whitney.purdum@gmail.com>
2016-04-18 19:23:01 -0400

    event 7

--- js/index.js
+++ js/index.js
@@ -5,39 +5,6 @@ $(document).ready(function() {
 			$(".hidden-list").css('display','block');
 		}
 
-
-
-
-
-
-
-			function processForm(event) {
-		event.preventDefault();
-
-
-		var emailAddress = $('input[name="emailAddress"]').val();
-		var lovesSpace = $('input[name="lovesSpace"]:checked').val();
-		var numberOfAstronauts = $('input[name="astronauts"]:checked').length;
-		var planet = $('select[name="planet"]').val();
-
-		if (username === '') {
-			$('#errorMessage')
-				.html('Please enter a username')
-				.removeClass('hide');
-		}
-
-		else if (password.length < 8) {
-			$('#errorMessage')
-				.html('Password must be 8 characters')
-				.removeClass('hide');
-		}
-
-
-		else if (emailAddress.indexOf('@') === -1) {
-			$('#errorMessage')
-				.html('Please provide a valid email address')
-				.removeClass('hide');
-
 	});
 
  
